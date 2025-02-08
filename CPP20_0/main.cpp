aa954515a672027ab7f83030194fba8daa120206
sas26031962 <sasx26031962@gmail.com>
2025-02-08 18:30:36 +0300

    Добавление папки CPP20_0

---
+++ CPP20_0/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::vector<int> numbers = {5, 2, 8, 1, 9};
+    std::ranges::sort(numbers); // Используем ranges::sort из C++20
+
+    std::cout << "Sorted numbers: ";
+    for (int num : numbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
